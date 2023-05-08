180c2576f46c7ca391d7184f7c874bb9def8d19f
Ggngr <125410954+Ggngr@users.noreply.github.com>
2023-05-08 14:51:19 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let direction = 0
+input.calibrateCompass()
+basic.forever(function () {
+    direction = input.compassHeading()
+    if (direction < 45) {
+        basic.showString("N")
+    } else if (direction < 135) {
+        basic.showString("E")
+    } else if (direction < 225) {
+        basic.showString("S")
+    } else {
+        basic.showString("W")
+    }
+})
